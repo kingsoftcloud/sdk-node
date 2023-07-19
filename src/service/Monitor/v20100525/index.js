d4e6ed3b2a0a6bb890cefe05f8e4ee7d614e9b2a
FengYikai <feng_yikai@foxmail.com>
2023-07-19 10:11:40 +0800

    Update SDK

---
+++ src/service/Monitor/v20100525/index.js
@@ -0,0 +1,42 @@
+const BaseClient = require("../../../base/BaseClient.js");
+
+module.exports = class Client extends BaseClient {
+    _baseConfig = {
+        'protocol': 'http://',
+        'endpoint': 'monitor.api.ksyun.com',
+        'config': {
+            'timeout': 60,  //设置timeout
+            'headers': {
+                'Accept': 'application/json'
+            },
+            'credentials': {
+                'region': 'cn-shanghai-3',
+                'service': 'monitor',
+            },
+        },
+    }
+    _apiList = {
+        'GetMetricStatistics': {
+            'url': '/',
+            'method': 'GET',
+            'config': {
+                'query': {
+                    'Version': '2010-05-25',
+                    'Action': 'GetMetricStatistics',
+                },
+                'headers': {
+                    'Content-Type': 'application/json'
+                },
+            },
+            'paramsType': {
+                'Namespace': 'String',
+                'InstanceID': 'String',
+                'MetricName': 'String',
+                'StartTime': 'String',
+                'EndTime': 'String',
+                'Aggregate': 'String',
+                'Period': 'Int',
+            }
+        },
+    }
+}
