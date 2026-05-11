3c8510aaca97eb16c27c103c44ef4732ca581d9c
fengyikai <fengyikai@kingsoft.com>
2026-05-11 14:34:48 +0800

    金山云码(kscc)：KSCC支持OpenApi方式的操作接口，包括：配额、日志等；

--- src/service/Kscc/v1/index.js
+++ src/service/Kscc/v1/index.js
@@ -47,6 +47,296 @@ module.exports = class Client extends BaseClient {
       },
       paramsType: {
         UserName: "String",
+        Month: "String",
+      },
+    },
+    DescribeAiLogDetailByIds: {
+      url: "/",
+      method: "GET",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DescribeAiLogDetailByIds",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      paramsType: {
+        MessageIds: "String",
+      },
+    },
+    DescribeAiLogDetail: {
+      url: "/",
+      method: "GET",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DescribeAiLogDetail",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      paramsType: {
+        StartTime: "String",
+        EndTime: "String",
+        UserName: "String",
+        ModelList: "String",
+        Page: "Int",
+        Size: "Int",
+      },
+    },
+    DescribeOrganizationTree: {
+      url: "/",
+      method: "GET",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DescribeOrganizationTree",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      paramsType: {},
+    },
+    DescribeModelMetrics: {
+      url: "/",
+      method: "GET",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DescribeModelMetrics",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      paramsType: {
+        StartTime: "String",
+        EndTime: "String",
+        ModelName: "String",
+        TimeInterval: "String",
+      },
+    },
+    DescribeUserTokenUsage: {
+      url: "/",
+      method: "GET",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DescribeUserTokenUsage",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      paramsType: {
+        StartTime: "String",
+        EndTime: "String",
+        UserName: "String",
+        Department: "String",
+      },
+    },
+    DescribeUserQuotaList: {
+      url: "/",
+      method: "GET",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DescribeUserQuotaList",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      paramsType: {
+        Keyword: "String",
+        PageNum: "Int",
+        PageSize: "Int",
+        Month: "String",
+        SortKey: "String",
+        SortType: "String",
+      },
+    },
+    DescribeQuotaGlobalConfig: {
+      url: "/",
+      method: "GET",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DescribeQuotaGlobalConfig",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      paramsType: {},
+    },
+    UpdateQuotaGlobalConfig: {
+      url: "/",
+      method: "POST",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "UpdateQuotaGlobalConfig",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      paramsType: {
+        WarnThreshold: "Int",
+        Discount: "Double",
+        ContactsJson: "String",
+        AccountEnabled: "Int",
+        DeptEnabled: "Int",
+        MemberEnabled: "Int",
+        DefaultMemberQuota: "Double",
+      },
+    },
+    DescribeAccountQuota: {
+      url: "/",
+      method: "GET",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DescribeAccountQuota",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      paramsType: {},
+    },
+    UpdateAccountQuota: {
+      url: "/",
+      method: "POST",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "UpdateAccountQuota",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      paramsType: {
+        QuotaAmount: "Double",
+      },
+    },
+    DeleteAccountQuota: {
+      url: "/",
+      method: "POST",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DeleteAccountQuota",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      paramsType: {},
+    },
+    UpdateDefaultMemberQuota: {
+      url: "/",
+      method: "POST",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "UpdateDefaultMemberQuota",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      paramsType: {
+        DefaultMemberQuota: "Double",
+      },
+    },
+    DeleteDefaultMemberQuota: {
+      url: "/",
+      method: "POST",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DeleteDefaultMemberQuota",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      paramsType: {},
+    },
+    DescribeDeptQuotaList: {
+      url: "/",
+      method: "GET",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DescribeDeptQuotaList",
+        },
+        headers: {
+          "Content-Type": "application/x-www-form-urlencoded",
+        },
+      },
+      paramsType: {
+        Keyword: "String",
+        PageNum: "Int",
+        PageSize: "Int",
+        SortKey: "String",
+        SortType: "String",
+      },
+    },
+    UpdateDeptQuota: {
+      url: "/",
+      method: "POST",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "UpdateDeptQuota",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      paramsType: {
+        DeptId: "String",
+        QuotaAmount: "Double",
+      },
+    },
+    DeleteDeptQuota: {
+      url: "/",
+      method: "POST",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DeleteDeptQuota",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      paramsType: {
+        DeptId: "String",
+      },
+    },
+    DeleteUserQuota: {
+      url: "/",
+      method: "POST",
+      config: {
+        query: {
+          Version: "V1",
+          Action: "DeleteUserQuota",
+        },
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      paramsType: {
+        UserName: "String",
       },
     },
   };
